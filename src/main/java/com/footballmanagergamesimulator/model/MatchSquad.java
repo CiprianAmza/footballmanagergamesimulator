398dde7627e3b369258d8a8813a09d7aa00ed660
ciprian.amza <ciprian.amza@everymatrix.com>
2023-04-15 16:04:02 +0300

    added playerNames for positionIds and tacticId for MatchSquadTacticRelation (todo)

--- src/main/java/com/footballmanagergamesimulator/model/MatchSquad.java
+++ src/main/java/com/footballmanagergamesimulator/model/MatchSquad.java
@@ -18,28 +18,47 @@ public class MatchSquad {
    * Relation ids
    */
   private long teamId;
+  private long tacticId;
 
   /**
-   * 18 positions (11 first team positions + 7 substitues)
+   * 18 positions (11 first team positions + 7 substitues) playerId + playerName
    */
-  private long position1;
-  private long position2;
-  private long position3;
-  private long position4;
-  private long position5;
-  private long position6;
-  private long position7;
-  private long position8;
-  private long position9;
-  private long position10;
-  private long position11;
-  private long position12;
-  private long position13;
-  private long position14;
-  private long position15;
-  private long position16;
-  private long position17;
-  private long position18;
+  private long positionPlayerId1;
+  private String positionPlayerName1;
+  private long positionPlayerId2;
+  private String positionPlayerName2;
+  private long positionPlayerId3;
+  private String positionPlayerName3;
+  private long positionPlayerId4;
+  private String positionPlayerName4;
+  private long positionPlayerId5;
+  private String positionPlayerName5;
+  private long positionPlayerId6;
+  private String positionPlayerName6;
+  private long positionPlayerId7;
+  private String positionPlayerName7;
+  private long positionPlayerId8;
+  private String positionPlayerName8;
+  private long positionPlayerId9;
+  private String positionPlayerName9;
+  private long positionPlayerId10;
+  private String positionPlayerName10;
+  private long positionPlayerId11;
+  private String positionPlayerName11;
+  private long positionPlayerId12;
+  private long positionPlayerName12;
+  private long positionPlayerId13;
+  private String positionPlayerName13;
+  private long positionPlayerId14;
+  private String positionPlayerName14;
+  private long positionPlayerId15;
+  private String positionPlayerName15;
+  private long positionPlayerId16;
+  private String positionPlayerName16;
+  private long positionPlayerId17;
+  private String positionPlayerName17;
+  private long positionPlayerId18;
+  private String positionPlayerName18;
 
 
 
