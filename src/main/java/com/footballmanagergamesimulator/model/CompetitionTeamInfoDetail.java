ae314a4b34d8f4a05bee264e7776cfa773267983
ciprian.amza <ciprian.amza@everymatrix.com>
2023-04-17 22:38:54 +0300

    added CompetitionTeamInfoDetail

---
+++ src/main/java/com/footballmanagergamesimulator/model/CompetitionTeamInfoDetail.java
@@ -0,0 +1,31 @@
+package com.footballmanagergamesimulator.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class CompetitionTeamInfoDetail {
+
+  @Id
+  @GeneratedValue(strategy= GenerationType.IDENTITY)
+  private long id;
+
+  /**
+   * Relation ids
+   */
+  private long team1Id;
+  private long team2Id;
+  private long competitionId;
+
+  /**
+   * Score Info
+   */
+
+  private String score;
+
+
+}
