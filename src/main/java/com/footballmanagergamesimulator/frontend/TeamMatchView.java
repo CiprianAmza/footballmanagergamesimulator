01bf99f59a73fa2aa45bcdb36cd9b6cd215baf35
ciprian.amza <ciprian.amza@everymatrix.com>
2023-04-18 20:01:58 +0300

    added TeamMatchView to display score and team names in frontend

---
+++ src/main/java/com/footballmanagergamesimulator/frontend/TeamMatchView.java
@@ -0,0 +1,11 @@
+package com.footballmanagergamesimulator.frontend;
+
+import lombok.Data;
+
+@Data
+public class TeamMatchView {
+
+  private String teamName1;
+  private String teamName2;
+  private String score;
+}
