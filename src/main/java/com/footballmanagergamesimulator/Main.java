33ab9c4e83fea2e81772666ad4c6d3a63abbdb77
ciprian.amza <ciprian.amza@everymatrix.com>
2023-04-18 20:02:50 +0300

    enabled scheduling

--- src/main/java/com/footballmanagergamesimulator/Main.java
+++ src/main/java/com/footballmanagergamesimulator/Main.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableKafka
+@EnableScheduling
 public class Main {
 
     public static void main(String[] args) {
