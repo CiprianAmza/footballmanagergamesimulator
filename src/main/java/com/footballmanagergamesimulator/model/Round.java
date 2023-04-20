7db5a490c50c8b0f943b672e3b0ea37eddeaf272
ciprian.amza <ciprian.amza@everymatrix.com>
2023-04-20 22:19:21 +0300

    added seasonNumber parameter

--- src/main/java/com/footballmanagergamesimulator/model/Round.java
+++ src/main/java/com/footballmanagergamesimulator/model/Round.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Round {
   public long round = 1L;
+  public long season = 1L;
 
   public long getRound() {
     return round;
@@ -12,5 +13,13 @@ public class Round {
 
   public void setRound(long round) {
     this.round = round;
+  }
+
+  public long getSeason() {
+    return season;
+  }
+
+  public void setSeason(long season) {
+    this.season = season;
   }
 }
