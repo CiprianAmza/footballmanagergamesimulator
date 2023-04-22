35a779a93e44b13e098c41a88e4b161785fb76d8
ciprian.amza <ciprian.amza@everymatrix.com>
2023-04-22 16:50:18 +0300

    added NameGenerator

--- src/main/java/com/footballmanagergamesimulator/nameGenerator/NameGenerator.java
+++ src/main/java/com/footballmanagergamesimulator/nameGenerator/NameGenerator.java
@@ -1,4 +1,28 @@
 package com.footballmanagergamesimulator.nameGenerator;
 
+import java.util.List;
+import java.util.Random;
+
 public class NameGenerator {
+
+  private static List<String> SUFFIXES = List.of("ius", "us", "sus", "sius");
+  private static List<String> MIDDLE = List.of("p", "n", "an", "rp");
+  private static List<String> VOWELS = List.of("a", "e", "i", "o", "u");
+  private static List<String> PREFIXES = List.of("Ex", "Aec", "Oc", "Eol", "Ir", "Uv", "Uk", "Ux", "Ax", "Ox", "Ix", "Iec", "Iol");
+
+
+  public static String generateName() {
+
+    StringBuilder name = new StringBuilder();
+    Random random = new Random();
+    name.append(PREFIXES.get(random.nextInt(PREFIXES.size())));
+    name.append(VOWELS.get(random.nextInt(VOWELS.size())));
+    name.append(MIDDLE.get(random.nextInt(MIDDLE.size())));
+    name.append(SUFFIXES.get(random.nextInt(SUFFIXES.size())));
+
+
+    return name.toString();
+  }
+
+
 }
