aa9cff330e82d60b812dd3ec9a2ace3ce62abc6c
ciprian.amza <ciprian.amza@everymatrix.com>
2023-05-10 14:27:37 +0300

    modified HumanRepository

--- src/main/java/com/footballmanagergamesimulator/repository/HumanRepository.java
+++ src/main/java/com/footballmanagergamesimulator/repository/HumanRepository.java
@@ -1,10 +1,16 @@
 package com.footballmanagergamesimulator.repository;
 
 import com.footballmanagergamesimulator.model.Human;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HumanRepository extends JpaRepository<Human, Long> {
+
+    List<Human> findAllByTeamId(long teamId);
+    List<Human> findAllByTeamIdAndTypeId(long teamId, long typeId);
 
 }
