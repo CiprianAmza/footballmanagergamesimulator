642ff1d1a0dea7a705ea9fdc670a931af9a9a0c1
ciprian.amza <ciprian.amza@everymatrix.com>
2023-06-29 22:17:12 +0300

    ...

---
+++ src/main/java/com/footballmanagergamesimulator/transfermarket/AcademyTransferStrategy.java
@@ -0,0 +1,9 @@
+package com.footballmanagergamesimulator.transfermarket;
+
+public class AcademyTransferStrategy extends AbstractTransferStrategy {
+
+    @Override
+    public void playersToSell() {
+
+    }
+}
