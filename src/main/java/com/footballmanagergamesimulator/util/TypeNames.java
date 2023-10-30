80be9ab4ebaf441de4042d07858c6901daa4803c
Ciprian Amza <ciprian.amza@everymatrix.com>
2023-10-30 21:30:05 +0200

    added TypeNames class

--- src/main/java/com/footballmanagergamesimulator/util/TypeNames.java
+++ src/main/java/com/footballmanagergamesimulator/util/TypeNames.java
@@ -1,7 +1,7 @@
 package com.footballmanagergamesimulator.util;
 
 public class TypeNames {
-
   public static final Long TEAM_TYPE = 1L;
   public static final Long HUMAN_TYPE = 2L;
+  public static final Long MATCH_TYPE = 3L;
 }
