c3eddc5251d66dcc3dc09d00f1a8a2a70513fa45
Ciprian Amza <ciprian.amza@everymatrix.com>
2023-11-07 21:03:17 +0200

    added TypeNames class

--- src/main/java/com/footballmanagergamesimulator/transfermarket/TransferStrategyUtil.java
+++ src/main/java/com/footballmanagergamesimulator/transfermarket/TransferStrategyUtil.java
@@ -5,4 +5,5 @@ public class TransferStrategyUtil {
   public static final long TRANSFER_STRATEGY_ACADEMY = 1L;
   public static final long TRANSFER_STRATEGY_BUY_YOUNG_SELL_HIGH = 2L;
   public static final long TRANSFER_STRATEGY_BUY_FREE_SELL_HIGH = 3L;
+  public static final long TRANSFER_STRATEGY_BUY_MID_SELL_MID = 4L;
 }
