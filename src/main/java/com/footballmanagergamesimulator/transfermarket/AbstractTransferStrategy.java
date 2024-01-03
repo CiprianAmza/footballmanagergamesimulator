fd71ba8a7385f0305296d35c44194fc9655ad0d7
Ciprian Amza <ciprian.amza@everymatrix.com>
2024-01-03 17:23:13 +0200

    changed main idea

--- src/main/java/com/footballmanagergamesimulator/transfermarket/AbstractTransferStrategy.java
+++ src/main/java/com/footballmanagergamesimulator/transfermarket/AbstractTransferStrategy.java
@@ -3,12 +3,13 @@ package com.footballmanagergamesimulator.transfermarket;
 import com.footballmanagergamesimulator.model.Team;
 import com.footballmanagergamesimulator.repository.HumanRepository;
 
+import java.util.HashMap;
 import java.util.List;
 
 public abstract class AbstractTransferStrategy implements TransferStrategy {
 
   @Override
-  public List<PlayerTransferView> playersToSell(Team team, HumanRepository humanRepository) {
+  public List<PlayerTransferView> playersToSell(Team team, HumanRepository humanRepository, HashMap<String, Integer> minimumPositionNeeded) {
 
     return null;
   }
