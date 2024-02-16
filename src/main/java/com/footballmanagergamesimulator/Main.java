ee0982ce620be3b3c074a4123e2f63a0c80b4cf1
Ciprian Amza <73443976+CiprianAmza@users.noreply.github.com>
2024-02-16 22:29:36 +0200

    Update Main.java

--- src/main/java/com/footballmanagergamesimulator/Main.java
+++ src/main/java/com/footballmanagergamesimulator/Main.java
@@ -4,12 +4,10 @@ package com.footballmanagergamesimulator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableKafka
 @EnableScheduling
 public class Main {
 
